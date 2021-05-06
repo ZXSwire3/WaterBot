f8a2822e4db98f3fc5ab7b81783b9535ca2648a4
Ben Foltz <eggie.foltz@gmail.com>
2021-05-06 00:16:19 -0700

    - fixed the formatting for the unrecognized command entry - added ~stop to the help menu. - removed useless parameter in hydrate() and hydrateTime() - added token file to just be required in the same folder as the .jar file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,8 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import javax.security.auth.login.LoginException;
-import java.io.InputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executors;
@@ -19,18 +20,25 @@ public class Main extends ListenerAdapter {
     private final ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
 
     public static void main(String[] args) throws LoginException {
-        InputStream file = Main.class.getResourceAsStream("token.txt");
+
+        //InputStream file = Main.class.getResourceAsStream("token.txt");
+        File file = new File("token.txt");
         String token;
-        Scanner fileReader = new Scanner(file);
-        if (!fileReader.hasNextLine()) {
-            System.err.println("No token entered. " +
-                    "\nPlease enter the bot's token file in token.txt file");
-        } else {
-            token = fileReader.nextLine();
-            JDABuilder builder = JDABuilder.createDefault(token);
-            builder.addEventListeners(new Main());
-            builder.build();
-            System.out.println("Ready to HYDRATE!");
+        try {
+            Scanner fileReader = new Scanner(file);
+            if (!fileReader.hasNextLine()) {
+                System.err.println("No token entered. " +
+                        "\nPlease enter the bot's token file in token.txt file");
+            } else {
+                token = fileReader.nextLine();
+                JDABuilder builder = JDABuilder.createDefault(token);
+                builder.addEventListeners(new Main());
+                builder.build();
+                System.out.println("Ready to HYDRATE!");
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Token file not found\n" +
+                                "Please put a token.txt file in the same directory as the .jar");
         }
     }
 
@@ -46,7 +54,7 @@ public class Main extends ListenerAdapter {
 
         if (msg.getContentRaw().contains(call + "")) {
             if (msg.getContentRaw().equalsIgnoreCase(call + "hydrate")|| msg.getContentRaw().equalsIgnoreCase(call + "h")) {
-                hydrate(channel, author, msg);
+                hydrate(channel, author);
             } else if (msg.getContentRaw().equalsIgnoreCase(call + "hTime")) {
                 channel.sendMessage("invalid choice, must be input as \"~htime time\"\n" +
                         "for example \"~htime 30\"\n" +
@@ -58,31 +66,32 @@ public class Main extends ListenerAdapter {
                 int time = Integer.parseInt(input[1]);
                 System.out.println(time);
                 //String units = input[2];
-                hydrateTime(channel, author, msg, time);
+                hydrateTime(channel, author, time);
 
             } else if (msg.getContentRaw().equalsIgnoreCase(call + "stop")) {
                 peopleTime.remove(author);
-                channel.sendMessage(author + "will no longer be reminded to drink water").queue();
+                channel.sendMessage(author + " will no longer be reminded to drink water").queue();
             } else if (msg.getContentRaw().equalsIgnoreCase(call + "help")) {
                 channel.sendMessage(">>> Welcome to **__hydration bot!!!__**\n" +
                         "The following are all valid commands: \n" +
                         "**~hydrate** or **~h**: Reminds you to drink some water every 30 min\n" +
                         "**~htime (time)**: Reminds you to drink some water at a given interval in minutes\n" +
-                        "**~help**: HEY! that's this command :D").queue();
+                        "**~help**: HEY! that's this command :D\n" +
+                        "**~stop**: Stops reminding you to drink water").queue();
             } else if (msg.getContentRaw().startsWith(String.valueOf(call))) {
                 channel.sendMessage(">>> Command unrecognized\n" +
-                                         "Type **__~help**__ for commands").queue();
+                                         "Type **__~help__** for commands").queue();
 
             }
         }
 
     }
 
-    public void hydrate(MessageChannel channel, String author, Message msg) {
-        hydrateTime(channel, author, msg, 30);
+    public void hydrate(MessageChannel channel, String author) {
+        hydrateTime(channel, author, 30);
     }
 
-    public void hydrateTime(MessageChannel channel, String author, Message msg, int time) {
+    public void hydrateTime(MessageChannel channel, String author, int time) {
         if (peopleTime.putIfAbsent(author, time) == null) {
             HydrationTask test = new HydrationTask(channel, author, executor, peopleTime);
 
